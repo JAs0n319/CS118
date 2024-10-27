de3b47938bf8a8dff9c1404f7c5b879c2b259fc9
JAs0n <jiaxuan.zhou@warwick.ac.uk>
2024-10-27 17:08:07 +0000

    Broken新增两个函数 编程风格标准化

--- Broken.java
+++ Broken.java
@@ -1,31 +1,50 @@
+
+import java.nio.ReadOnlyBufferException;
 import uk.ac.warwick.dcs.maze.logic.IRobot;
 
-public class Broken 
-{
+public class Broken {
 
-     private byte isTargetNorth(IRobot robot) {
-          return (robot.getLocation().y < robot.getTargetLocation().y)?(byte)1:(byte)-1;
-     }
+  private byte isTargetNorth(int robotY, int targetY) {
+    if (robotY > targetY) {
+      return 1;
+    } else if (robotY < targetY) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 
-     public void controlRobot(IRobot robot) {
-          int randno;
-          
-          direction = IRobot.AHEAD;
-          
-          do {
-               randno = (int) Math.round(Math.random()*3);
-               if ( randno == 0)
-                    direction = IRobot.LEFT;
-               else if (randno == 1)
-                    direction = IRobot.RIGHT;
-               else if (randno == 2)
-                    direction = IRobot.BEHIND;
-               else 
-                    direction = IRobot.AHEAD;
-          } while (robot.look(direction)==IRobot.WALL);
-          
-          robot.face(direction);  /* Face the direction */   
+  private byte isTargetEast(int robotX, int targetX) {
+    if (robotX > targetX) {
+      return -1;
+    } else if (robotX < targetX) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 
-     }
+  public void controlRobot(IRobot robot) {
+    int randno;
+    int direction = IRobot.AHEAD;
+    
+    do {
+      randno = (int) Math.round(Math.random() * 3);
+      if (randno == 0) {
+        direction = IRobot.LEFT;
+      } else if (randno == 1) {
+        direction = IRobot.RIGHT;
+      } else if (randno == 2) {
+        direction = IRobot.BEHIND;
+      } else {
+        direction = IRobot.AHEAD;
+      }
+    } while (robot.look(direction) == IRobot.WALL);
+
+    System.out.println(isTargetEast(robot.getLocation().x,robot.getTargetLocation().x));
+    System.out.println(isTargetNorth(robot.getLocation().y,robot.getTargetLocation().y));
+
+    robot.face(direction); //Face the direction
+  }
 
 }
